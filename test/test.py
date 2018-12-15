cc9bfccb6ca641c135903166df697416a7041513
david <david@shoplineapp.com>
2018-12-15 21:44:38 +0800

    feat(unittest): add find cluster test and gitignore

--- test/test.py
+++ test/test.py
@@ -1,5 +1,7 @@
 import unittest
+import json
 import lambda_function
+from ecs import FindClusterName, EcsCluster
 
 class test_handler(unittest.TestCase):
     def test_event(self):
@@ -7,17 +9,24 @@ class test_handler(unittest.TestCase):
             "Records":
             [
                 {
-                    "body": {
+                    "body": """{
                         "LifecycleHookName": "scale-in-lifecycle", 
                         "AutoScalingGroupName": "EC2ContainerService-api-production-asg", 
                         "EC2InstanceId": "i-0012362652ecc7ee4"
-                        }
+                        }"""
                     }
                 ]
             }
 
 
-        self.assertEqual(lambda_function.event_handler(test_event), (['EC2ContainerService-api-production-asg', 'api-production', 'i-0012362652ecc7ee4']))
+        self.assertEqual(lambda_function.event_handler(test_event), (['scale-in-lifecycle', 'EC2ContainerService-api-production-asg', 'i-0012362652ecc7ee4']))
+
+
+    def test_find_cluster_name(self):
+        ## shoplytics-preview instance, test may be fail when this instance no longer exist ### TODO: NEED TO FIX
+        to_be_drain_instance_id = 'i-02720f0c3f9be97a1'
+
+        self.assertEqual(FindClusterName(to_be_drain_instance_id).find_cluster_name(), ('shoplytics-preview'))
 
 
         
