39e29a60cc3460ea76e46dee2ea963fa6bdb7969
david <david@shoplineapp.com>
2018-12-16 21:29:25 +0800

    feat(terraform): add policy which is allow send to sqs

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -3,5 +3,6 @@ resource "aws_sqs_queue" "lifecycle-scale-in" {
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 0
+  policy                    = "${file("sqs-policy.json")}"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,6 +2,10 @@ variable "AWS_REGION" {
   default = "ap-southeast-1"
 }
 
-variable "asg_name" {}
+variable "asg_name" {
+  default = "EC2ContainerService-api-production-asg"
+}
 
-variable "account_id" {}
+variable "account_id" {
+  default = "990090895087"
+}
