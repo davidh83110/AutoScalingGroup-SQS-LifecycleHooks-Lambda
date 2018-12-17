682a9a54c026223c08761851d28a6895d82a4dc4
david <david@shoplineapp.com>
2018-12-17 12:48:35 +0800

    feat(terraform): rename lambda and change lang to python3.6

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,14 +1,8 @@
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "lifecycle-sqs" {
   filename         = "lambda_function_payload.zip"
   function_name    = "lambda_function_name"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "exports.test"
   source_code_hash = "${base64sha256(file("../lambda_function_payload.zip"))}"
-  runtime          = "nodejs8.10"
-
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
+  runtime          = "python3.6"
 }
