a08fe9e4aed61defd5c6b142216007e1139ce2e2
david <david@shoplineapp.com>
2018-12-17 12:59:08 +0800

    feat(terraform): fix lambda arguments

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "lifecycle-sqs" {
   filename         = "lambda_function_payload.zip"
-  function_name    = "lambda_function_name"
+  function_name    = "AutoScalingGroup-SQS-LifecycleHooks"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "exports.test"
+  handler          = "lambda_function.lambda_handler"
   source_code_hash = "${base64sha256(file("../lambda_function_payload.zip"))}"
   runtime          = "python3.6"
 }
